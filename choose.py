efef8c305004c1058bee9ef4fb3f79d88783e597
kunonononano <twitter.splatoon@gmail.com>
2019-09-24 00:27:08 +0900

    mergeミスの修正

--- choose.py
+++ choose.py
@@ -13,20 +13,17 @@ async def on_message(message):
         await do_separate(message)
     elif message.content.startswith('/emoji'):
         await create_emoji(message)
-<<<<<<< HEAD
     elif message.content.startswith('/how2'):
         await client.send_message(message.channel,
                                   '\"/sep 数字\"と打つと打った人のいるvoice channelの中でランダムに指定した数字の人数が選ばれます。\n観戦者を分けるときにでもどうぞ\n'
                                   + '画像を一枚添付して\"emoji 名前\"と打つと選んだ画像が指定した名前で絵文字になります。\n'
                                   + '絵文字は合計50しか保存できないので管理者の方は注意してね')
-=======
     elif message.content.startswith('/quiz'):
         await create_quiz(message)
     elif message.content.startswith('/ans'):
         await judge_answer(message)
     elif message.content.startswith('/add'):
         await add_abbreviation(message)
->>>>>>> 26ff6872e369c3c22be000f3c164547cbcfae335
     # elif message.content.startswith('/exit'):
     #     reply = 'ばいば～い'
     #     await client.send_message(message.channel, reply)
@@ -70,9 +67,6 @@ async def create_emoji(message):
     img_read = urllib.request.urlopen(request).read()
     emoji = await client.create_custom_emoji(message.server, name=name, image=img_read)
 
-<<<<<<< HEAD
-client.run('NTIxNjQ2MTcyNjA4MTM1MTk3.DvCJ8g.qwopdB1kpdUvtvHbd1KJYCTbnhQ')
-=======
 @client.event
 async def create_quiz(message):
     # TODO: 機能が未実装なためhow2を表示
@@ -108,4 +102,3 @@ async def add_abbreviation(message):
     cmd, formal, abbreviation = cmd
 
 client.run('NTIxNjQ2MTcyNjA4MTM1MTk3.Du_cfQ.L0aohITRdTXLj0_QP_m3c32u_X8')
->>>>>>> 26ff6872e369c3c22be000f3c164547cbcfae335
