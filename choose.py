26ff6872e369c3c22be000f3c164547cbcfae335
kunonononano <45773717+kunonononano@users.noreply.github.com>
2019-09-23 23:59:37 +0900

    [#1] コマンドの追加(機能は未実装)

--- choose.py
+++ choose.py
@@ -13,6 +13,12 @@ async def on_message(message):
         await do_separate(message)
     elif message.content.startswith('/emoji'):
         await create_emoji(message)
+    elif message.content.startswith('/quiz'):
+        await create_quiz(message)
+    elif message.content.startswith('/ans'):
+        await judge_answer(message)
+    elif message.content.startswith('/add'):
+        await add_abbreviation(message)
     # elif message.content.startswith('/exit'):
     #     reply = 'ばいば～い'
     #     await client.send_message(message.channel, reply)
@@ -23,7 +29,7 @@ async def on_message(message):
 @client.event
 async def do_separate(message):
     cmd = message.content.split(' ')
-    if len(cmd) !=2 or not cmd[1].isdecimal():
+    if len(cmd) != 2 or not cmd[1].isdecimal():
         await client.send_message(message.channel, 'コマンド/sepは\"/sep 数字\"の形で使ってください。')
         return
     members = []
@@ -37,7 +43,7 @@ async def do_separate(message):
 @client.event
 async def create_emoji(message):
     cmd = message.content.split(' ')
-    if len(cmd) !=2:
+    if len(cmd) != 2:
         await client.send_message(message.channel, 'コマンド/emojiは\"/emoji 名前\"の形で使ってください')
         return
     if len(cmd[1]) < 2:
@@ -55,5 +61,39 @@ async def create_emoji(message):
     request = urllib.request.Request(url=images[0]['url'], headers=headers)
     img_read = urllib.request.urlopen(request).read()
     emoji = await client.create_custom_emoji(message.server, name=name, image=img_read)
+
+@client.event
+async def create_quiz(message):
+    # TODO: 機能が未実装なためhow2を表示
+    await client.send_message(message.channel, 'コマンド/quizは\"/quiz\"または\"/quiz 武器種(スペース区切りで複数可能)\"の形で使ってください')
+    return
+    cmd = message.content.split(' ')
+    cmd, *weapon_types = cmd
+    
+    
+
+@client.event
+async def judge_answer(message):
+    # TODO: 機能が未実装なためhow2を表示
+    await client.send_message(message.channel, 'コマンド/ansは\"/ans (サブの名前) (スペシャルの名前)\"の形で答えてください')
+    return
+    cmd = message.content.split(' ')
+    # TODO: サブウェポン、スペシャルのリストを作ったらスペシャル、サブの順番で答えてるのもここに来るようにする
+    if len(cmd) != 3:
+        await client.send_message(message.channel, 'コマンド/ansは\"/ans (サブの名前) (スペシャルの名前)\"の形で答えてください')
+        return
+    cmd, sub, special = cmd
+
+@client.event
+async def add_abbreviation(message):
+    # TODO: 機能が未実装なためhow2を表示
+    await client.send_message(message.channel, 'コマンド/addは\"/add (正式名称) (略称)\"の形で追加してください\nサブウェポン、スペシャルウェポンの略称を登録することができます')
+    return
+    cmd = message.content.split(' ')
+    # TODO: サブウェポン、スペシャルのリストを作ったらスペシャル、サブの順番で答えてるのもここに来るようにする
+    if len(cmd) != 3:
+        await client.send_message(message.channel, 'コマンド/addは\"/add (正式名称) (略称)\"の形で追加してください\nサブウェポン、スペシャルウェポンの略称を登録することができます')
+        return
+    cmd, formal, abbreviation = cmd
 
 client.run('NTIxNjQ2MTcyNjA4MTM1MTk3.Du_cfQ.L0aohITRdTXLj0_QP_m3c32u_X8')
